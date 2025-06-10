185d16f71fb0446fc0abccc790ab487d847f48d6
RKN <beingrkn@gmail.com>
2025-06-10 21:53:34 +0530

    Create theme.js

---
+++ js/theme.js
@@ -0,0 +1,6 @@
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    themeToggle.textContent = isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode';
+});
